31bcdcc2abf0d02a78510914ebf28ba568542254
reem <Reem.Fawaz.ruh.k@tuwaiq.edu.sa>
2021-11-05 16:05:05 +0300

    Update and some fixes

--- app/src/main/java/com/example/googleform/MainActivity.kt
+++ app/src/main/java/com/example/googleform/MainActivity.kt
@@ -2,13 +2,10 @@ package com.example.googleform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+ override fun onCreate(savedInstanceState: Bundle?) {
+  super.onCreate(savedInstanceState)
+  setContentView(R.layout.activity_main)
+ }
 }
